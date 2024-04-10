4377eadcb715019a205698a1b212bedbd5e0a86c
Nastya Sol <an5oldatova@ya.ru>
2024-04-10 12:46:07 +0300

    commit one is all work

---
+++ src/widget.py
@@ -0,0 +1,25 @@
+def masks_card_and_account(card_or_account):
+
+    """ Функцию, которая работает как с картами, так и со счетами и возвращает их маску"""
+
+    if card_or_account[:12] == "Visa Classic":
+        return f'{card_or_account[:17]} {card_or_account[17:19]}** **** {card_or_account[25:]}'
+
+    elif card_or_account[:9] == "Visa Gold":
+        return f'{card_or_account[:14]} {card_or_account[14:16]}** **** {card_or_account[22:]}'
+
+    elif card_or_account[:13] == "Visa Platinum":
+        return f'{card_or_account[:18]} {card_or_account[18:20]}** **** {card_or_account[26:]}'
+
+    elif card_or_account[:7] == "Maestro":
+        return f"{card_or_account[:13]} {card_or_account[12:14]}** **** {card_or_account[20:]}"
+
+    else:
+        return f"{card_or_account[:4]} **{card_or_account[21:]}"
+
+
+print(masks_card_and_account('Visa Gold 5999414228426353'))
+print(masks_card_and_account('Maestro 1596837868705199'))
+print(masks_card_and_account('Visa Classic 6831982476737658'))
+print(masks_card_and_account('Visa Platinum 8990922113665229'))
+print(masks_card_and_account('Счет 73654108430135874305'))
