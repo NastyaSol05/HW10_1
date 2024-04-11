6695e8d758c3395abe6d74b4d1470578cd34390f
Nastya Sol <an5oldatova@ya.ru>
2024-04-11 12:53:15 +0300

    make new function

--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,16 @@
+from src.masks import number_account, number_card
+
+
 def masks_card_and_account(card_or_account: str) -> str:
-
     """ Функцию, которая работает как с картами, так и со счетами и возвращает их маску"""
+    number = card_or_account.split(" ")[-1]
 
-    if card_or_account[:12] == "Visa Classic":
-        return f'{card_or_account[:17]} {card_or_account[17:19]}** **** {card_or_account[25:]}'
-
-    elif card_or_account[:9] == "Visa Gold":
-        return f'{card_or_account[:14]} {card_or_account[14:16]}** **** {card_or_account[22:]}'
-
-    elif card_or_account[:13] == "Visa Platinum":
-        return f'{card_or_account[:18]} {card_or_account[18:20]}** **** {card_or_account[26:]}'
-
-    elif card_or_account[:7] == "Maestro":
-        return f"{card_or_account[:13]} {card_or_account[12:14]}** **** {card_or_account[20:]}"
-
+    if "Счет" in card_or_account:
+        return f"Счет {number_account(number)}"
     else:
-        return f"{card_or_account[:4]} **{card_or_account[21:]}"
+        return f"{card_or_account.replace(number, "")}{number_card(number)}"
 
 
-def return_date(datetimes: str) -> str:
+def date_output(datetimes: str) -> str:
     """ Функция, которая возвращает строку с датой """
     return f'{datetimes[8:10]}.{datetimes[5:7]}.{datetimes[:4]}'
-
-
