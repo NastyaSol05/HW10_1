7fb6ab2aa2edc42b3c479bc0e995a385c46bd818
Nastya Sol <an5oldatova@ya.ru>
2024-04-24 14:11:07 +0300

    update naming

--- tests/tests_masks.py
+++ tests/tests_masks.py
@@ -8,8 +8,8 @@ def my_card() -> str:
     return "7000792289606361"
 
 
-def test_number_card(my_card: str) -> None:
-    assert number_card(my_card) == "7000 79** **** 6361"
+def test_number_card(by_card: str) -> None:
+    assert number_card(by_card) == "7000 79** **** 6361"
 
 
 @pytest.fixture
@@ -17,5 +17,5 @@ def my_account() -> str:
     return "73654108430135874305"
 
 
-def test_number_account(my_account: str) -> None:
-    assert number_account(my_account) == "**4305"
+def test_number_account(by_account: str) -> None:
+    assert number_account(by_account) == "**4305"
