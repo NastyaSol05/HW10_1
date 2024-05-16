84df39da4f254f79e2a39cac85e564655972596c
Nastya Sol <an5oldatova@ya.ru>
2024-05-16 18:20:48 +0300

    add tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,23 @@
+import json
+import unittest
 from unittest.mock import patch
 
 from src.utils import loads_json, operation_returns
+
+
+def test_loads_json() -> None:
+    """Тест функции loads_json"""
+
+    mock_json_data = {"ключ1": "поддельное значение1", "ключ2": "поддельное значение2"}
+
+    mock_open = unittest.mock.mock_open(read_data=json.dumps(mock_json_data))
+
+    with unittest.mock.patch("builtins.open", mock_open), unittest.mock.patch("os.stat") as mock_stat:
+        mock_stat.return_value.st_size = 2
+
+        data = loads_json("data.json")
+
+    assert data == mock_json_data
 
 
 @patch("requests.get")
